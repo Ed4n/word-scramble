db40ea15149c0a593156b9c18860ef3e432d1cd2
Edgardo Valencia <edgardo.avalencia@gmail.com>
2024-11-18 11:43:27 -0600

    Initial Commit

---
+++ WordsScramble/WordsScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsScrambleApp.swift
+//  WordsScramble
+//
+//  Created by Edgardo Valencia on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WordsScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordsScrambleTests/WordsScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordsScrambleTests.swift
+//  WordsScrambleTests
+//
+//  Created by Edgardo Valencia on 18/11/24.
+//
+
+import Testing
+@testable import WordsScramble
+
+struct WordsScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
